6451621d5c089686371b451f3f42cc1bf42d343c
Brandon_Pampuch <brandonpampuch@gmail.com>
2019-07-13 17:15:10 -0500

    add portfolio images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import Layout from "../layouts/Layout"
 import styles from "./index.module.scss"
+import TMSDNE from "../../public/assets/TMSDNE.png"
+import RS from "../../public/assets/RS.png"
 
 export default () => (
   <Layout>
@@ -13,13 +15,16 @@ export default () => (
       <div className={styles.border}></div>
       <div>
         <div class={styles.portfolio}>
-      <h2>TMSDE</h2>
-      <h2>Refugee Stories</h2>
-      <h2>Labs project</h2>
+          <div class={styles.card}>
+            <h2>TMSDE</h2>
+            <img src={TMSDNE} alt={"TMSDNE"}/>
+          </div>
+          <div class={styles.card}>
+            <h2>Refugee Stories</h2>
+            <img src={RS} alt={"Refugee Stories"}/>
+          </div>
+        </div>
       </div>
     </div>
-    </div>
-
- 
   </Layout>
 )
